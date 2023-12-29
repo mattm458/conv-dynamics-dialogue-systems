7878393ef85ad1fef62412518fb90115d56b2086
Matt <mattm458@gmail.com>
2023-12-29 13:07:57 -0500

    so much documentation

--- src/cdmodel/preprocessing/main.py
+++ src/cdmodel/preprocessing/main.py
@@ -16,24 +16,48 @@ from datetime import datetime
 from cdmodel.preprocessing.consts import FEATURES, FEATURES_NORM_BY_CONV_SPEAKER
 
 
-def _segment_export(data: DataFrame, out_dir: str) -> None:
-    segment_path: Final[str] = path.join(out_dir, "segments")
+def _segment_export(df: DataFrame, out_dir: str) -> None:
+    """
+    Export conversational segment data to Torch .pt files organized by conversation.
 
-    # Have we processed segment data already?
+    Parameters
+    ----------
+    df : DataFrame
+        A DataFrame containing conversational segment data to export.
+    out_dir : str
+        A path to the directory where segment data should be saved.
+    """
+
+    # If we performed segment export already, there will be a "segments" directory
+    # in the output directory. If not, we have to perform segment export.
+    segment_path: Final[str] = path.join(out_dir, "segments")
     if path.exists(segment_path):
         print("segments directory exists, skipping...")
         return
-    else:
-        os.mkdir(segment_path)
 
-    for id, group in tqdm(data.groupby("id"), desc="Exporting segments"):
+    os.mkdir(segment_path)
+
+    for id, group in tqdm(df.groupby("id"), desc="Exporting segments"):
         torch.save(group.to_dict(orient="list"), path.join(segment_path, f"{id}.pt"))
 
 
-def _embeddings(data: DataFrame, out_dir: str) -> None:
-    embeddings_path: Final[str] = path.join(out_dir, "embeddings")
+def __extract_embeddings(df: DataFrame, out_dir: str) -> None:
+    """
+    Perform embedding extraction on text transcripts contained in the given DataFrame.
 
-    # Have we processed embeddings already?
+    Currently, embedding extraction produces 50d GloVe embeddings.
+
+    Parameters
+    ----------
+    df : DataFrame
+        A DataFrame containing dialogue transcripts.
+    out_dir : str
+        A path to the directory where GloVe embeddings should be saved.
+    """
+
+    # If we performed embedding extraction already, there will be an "embedddings" directory
+    # in the output directory. If not, we have to perform embedding extraction.
+    embeddings_path: Final[str] = path.join(out_dir, "embeddings")
     if path.exists(embeddings_path):
         print("embeddings directory exists, skipping...")
         return
@@ -45,7 +69,7 @@ def _embeddings(data: DataFrame, out_dir: str) -> None:
     tokenizer = get_tokenizer("basic_english")
 
     # Look up and save embeddings for each conversation
-    for id, text in tqdm(data.groupby("id")["transcript"], desc="Exporting embeddings"):
+    for id, text in tqdm(df.groupby("id")["transcript"], desc="Exporting embeddings"):
         tokenized = [tokenizer(x) for x in text]
         vectorized = [vec.get_vecs_by_tokens(x) for x in tokenized]
         lengths = torch.LongTensor([len(x) for x in vectorized])
@@ -58,34 +82,77 @@ def _embeddings(data: DataFrame, out_dir: str) -> None:
         torch.save(lengths, path.join(embeddings_path, f"{id}-lengths.pt"))
 
 
-def _normalize(data: DataFrame, out_dir: str) -> DataFrame:
+def __normalize(df: DataFrame, out_dir: str) -> DataFrame:
+    """
+    Perform normalization on a given set of speech features in a DataFrame.
+
+    Currently, the following types of normalization are performed:
+
+    * 3 standard deviations above and below the median scaled to [-1, 1], per [Raitio et al., 2020](https://arxiv.org/abs/2009.06775).
+        * By speaker and conversation
+
+    A CSV file titled `data-norm.csv` containing the results of normalization
+    will be saved in the given `out_dir`.
+
+    Parameters
+    ----------
+    df : DataFrame
+        A DataFrame containing speech features to normalize
+    out_dir : str
+        A path to the directory where a CSV file should be saved.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame containing all original columns, plus new columns with normalized features.
+    """
+
     # If we performed normalization already, there will be a "data-norm.csv" file
-    # in the output directory. Otherwise, we have to perform normalization.
+    # in the output directory. If not, we have to perform normalization.
     data_norm_csv_path: Final[str] = path.join(out_dir, "data-norm.csv")
     if path.exists(data_norm_csv_path):
         print("data-norm.csv exists, loading...")
         return pd.read_csv(data_norm_csv_path)
 
-    data_norm = norm_by_conv_speaker(data, features=FEATURES)
+    # Perform normalization and save the results.
+    data_norm = norm_by_conv_speaker(df, features=FEATURES)
     data_norm.to_csv(data_norm_csv_path, index=False)
 
     return data_norm
 
 
-def _extract_features(dataset: Dataset, out_dir: str) -> DataFrame:
+def __extract_features(dataset: Dataset, out_dir: str) -> DataFrame:
+    """
+    Perform basic feature extraction on a given Dataset object.
+
+    A CSV file titled `data.csv` containing the results of feature extraction
+    will be saved in the given `out_dir`.
+
+    Parameters
+    ----------
+    dataset : Dataset
+        A Dataset object capable of performing feature extraction.
+    out_dir : str
+        A path to the directory where a CSV file should be saved.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame containing the results of preprocessing.
+    """
+
     # If we performed feature extraction already, there will be a "data.csv" file
-    # in the output directory. Otherwise, we have to perform feature extraction.
+    # in the output directory. If not, we have to perform feature extraction.
     data_csv_path: Final[str] = path.join(out_dir, "data.csv")
     if path.exists(data_csv_path):
         print("data.csv exists, loading...")
         return pd.read_csv(data_csv_path)
 
-    features = dataset.extract_features()
+    # Perform feature extraction and save the results
+    df = pd.DataFrame(dataset.extract_features())
+    df.to_csv(data_csv_path, index=False)
 
-    data = pd.DataFrame(features)
-    data.to_csv(data_csv_path, index=False)
-
-    return data
+    return df
 
 
 def preprocess(
@@ -104,7 +171,8 @@ def preprocess(
     dataset_dir : str
         Path to the dataset's root directory.
     out_dir : str, optional
-        Path to a directory where the results of preprocessing will be saved incrementally.
+        A path to the directory where the results of preprocessing will be saved incrementally.
+        If the directory does not exist, it will be created.
         If `None`, preprocessing output will be saved in `preprocessed/{dataset} {datetime}/`.
 
         If a preprocessing job was interrupted, and `out_dir` is set to the directory where the interrupted
@@ -146,14 +214,14 @@ def preprocess(
         print(f"Output directory {out_dir} already exists, resuming...")
 
     # Step 1: Basic feature extraction and normalization
-    data = _extract_features(dataset=dataset, out_dir=out_dir)
-    data_norm = _normalize(data=data, out_dir=out_dir)
+    data = __extract_features(dataset=dataset, out_dir=out_dir)
+    data_norm = __normalize(df=data, out_dir=out_dir)
 
     # Step 2: Embeddings
-    _embeddings(data=data_norm, out_dir=out_dir)
+    __extract_embeddings(df=data_norm, out_dir=out_dir)
 
     # Step 3: Segment export
-    _segment_export(data=data_norm, out_dir=out_dir)
+    _segment_export(df=data_norm, out_dir=out_dir)
 
     # ID assignment: All speakers
     torch.save(
